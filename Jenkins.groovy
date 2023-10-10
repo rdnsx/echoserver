d2e387c0dbc820bc2bfad3d16df8c0470abfcb45
MGebremski <m.gebremski@steinberg.de>
2023-10-10 10:46:28 +0200

    update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -14,21 +14,24 @@ pipeline {
         SOURCE_REPO_URL = "https://github.com/rdnsx/${DOMAIN}.git"
         DOCKER_IMAGE_NAME = "rdnsx/${STACK_NAME}"
     }
-      
-    stage('Deploy to Swarm') {
-        steps {
-            script {
-                sshagent(['Swarm00']) {
-                    sh """
-                        ssh -o StrictHostKeyChecking=no -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} '
-                        mount -a &&
-                        cd /mnt/SSS/DockerCompose/ &&
-                        rm -rf ${DOMAIN}/ &&
-                        mkdir ${DOMAIN}/ &&
-                        cd ${DOMAIN}/ &&
-                        curl -o docker-compose-swarm.yml https://raw.githubusercontent.com/rdnsx/${DOMAIN}/main/docker-compose-swarm.yml &&
-                        docker stack deploy -c docker-compose-swarm.yml ${STACK_NAME};'
-                    """
+    
+    stages {
+  
+        stage('Deploy to Swarm') {
+            steps {
+                script {
+                    sshagent(['Swarm00']) {
+                        sh """
+                            ssh -o StrictHostKeyChecking=no -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} '
+                            mount -a &&
+                            cd /mnt/SSS/DockerCompose/ &&
+                            rm -rf ${DOMAIN}/ &&
+                            mkdir ${DOMAIN}/ &&
+                            cd ${DOMAIN}/ &&
+                            curl -o docker-compose-swarm.yml https://raw.githubusercontent.com/rdnsx/${DOMAIN}/main/docker-compose-swarm.yml &&
+                            docker stack deploy -c docker-compose-swarm.yml ${STACK_NAME};'
+                            """
+                    }        
                 }
             }
         }
